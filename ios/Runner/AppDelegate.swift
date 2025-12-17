29ab17d5f06b4b91b3620fd07fd82de51778db66
melshenawyarabapps <m.elshenawy@arab-apps.com>
2025-12-17 19:41:56 +0200

    Add GoogleService-Info.plist and configure API keys
    Added GoogleService-Info.plist for iOS and updated AndroidManifest.xml and AppDelegate.swift with actual Google Maps API keys. Updated Xcode project to include the new plist file and adjusted code signing settings. These changes enable Google Maps and Firebase integration for both Android and iOS platforms.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
+    GMSServices.provideAPIKey("AIzaSyAZ6pTYx2IYaxa_9NRiHZ6goATYe8wgbx4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
